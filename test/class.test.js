608cb637d3ace7abbe7822bfba687afa73cb0d85
Matt Luedke <mluedke.ds@gmail.com>
2023-11-17 10:30:06 -0500

    fix: remove code that violates unsafe-eval CSP

--- test/class.test.js
+++ test/class.test.js
@@ -11,7 +11,7 @@ describe('Mode Parser', function() {
         return this.dancing;
       }
     });
-     
+
     var Ninja = Person.extend({
       init: function(){
         this._super( false );
@@ -24,60 +24,19 @@ describe('Mode Parser', function() {
         return true;
       }
     });
-     
-    var p = new Person(true);
-    p.dance().should.be.true;
-     
-    var n = new Ninja();
-    n.dance().should.be.false;
-    n.swingSword().should.be.true;
-     
-    // Should all be true
-    (p instanceof Person).should.be.true;
-    (p instanceof Class).should.be.true;
-    (n instanceof Ninja).should.be.true;
-    (n instanceof Person).should.be.true;
-    (n instanceof Class).should.be.true;
-  });
 
-  it('should have correct contructor name',function() {
-    var Person = Class.extend('Person', {
-      init: function(isDancing){
-        this.dancing = isDancing;
-      },
-      dance: function(){
-        return this.dancing;
-      }
-    });
-     
-    var Ninja = Person.extend('Ninja', {
-      init: function(){
-        this._super( false );
-      },
-      dance: function(){
-        // Call the inherited version of dance()
-        return this._super();
-      },
-      swingSword: function(){
-        return true;
-      }
-    });
-     
     var p = new Person(true);
     p.dance().should.be.true;
-     
+
     var n = new Ninja();
     n.dance().should.be.false;
     n.swingSword().should.be.true;
-     
+
     // Should all be true
     (p instanceof Person).should.be.true;
     (p instanceof Class).should.be.true;
     (n instanceof Ninja).should.be.true;
     (n instanceof Person).should.be.true;
     (n instanceof Class).should.be.true;
-	
-	p.constructor.name.should.be.equal('Person');
-	n.constructor.name.should.be.equal('Ninja');
   });
 });
